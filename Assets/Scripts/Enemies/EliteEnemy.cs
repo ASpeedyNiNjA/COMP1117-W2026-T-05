cf312627855826b21b9bc75cd82eca35e747638f
Chris Trombly <christrombly001@hotmail.com>
2026-02-04 04:09:34 -0500

    Enemy is double the size & blue, not moving

--- Assets/Scripts/Enemies/EliteEnemy.cs
+++ Assets/Scripts/Enemies/EliteEnemy.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public class EliteEnemy : Enemy
 {
     public SpriteRenderer superSprite;
+    public Transform transform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected override void Awake()
     
     {
         base.Awake();
-        superSprite = GetComponent<SpriteRenderer>();
+        superSprite = GetComponent<SpriteRenderer>(); //This line of code doesn't do anything and I'm not sure why, the SuperEagle still changes colour.
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         superSprite.color = Color.blue;
+        transform.localScale = new Vector3(2,2,2);
+        
         
     }
 }
