28c9b3ad36a63efacb7394399242b5676cf6b2d8
Chris Trombly <christrombly001@hotmail.com>
2026-02-11 01:36:08 -0500

    Sidequest 5B Complete

--- Assets/Scripts/Mechanics/Zones/TremorZone.cs
+++ Assets/Scripts/Mechanics/Zones/TremorZone.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TremorZone : Zone
 {
+
+    public UnityEvent shock;
+    private void Start()
+    {
+        InvokeRepeating("ShockwaveEvent", 1f, 2f);
+    }
+
     protected override void ApplyZoneEffect(Player player)
     {
         Debug.Log("Tremor Zone");
+        shock.Invoke();
     }
 }
